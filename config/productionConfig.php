8f7ef5620d2fc63e00fa8110226f14a1b7f08fe9
Bruno Morais <bruno@MBP13.local>
2023-11-20 19:17:44 -0300

    atualizacao database

--- config/productionConfig.php
+++ config/productionConfig.php
@@ -2,22 +2,25 @@
 // CONFIGURAÇÃO DO BANCO DE DADOS
 const CONFIG_DATA_LAYER = [
     "driver" => "mysql",
-    "host" => "localhost",
+    "host" => "mysql",
     "port" => "3306",
     "dbname" => "database",
-    "username" => "user",
-    "passwd" => '',
+    "username" => "root",
+    "passwd" => "root",
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8, lc_time_names = 'pt_BR'",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_CASE => PDO::CASE_NATURAL,
-        PDO::MYSQL_ATTR_FOUND_ROWS => true
+        PDO::MYSQL_ATTR_FOUND_ROWS => true,
+        PDO::ATTR_STRINGIFY_FETCHES => true
     ],
+    "homologation" => "homologacao",
+    "directory_models" => "App\\Models\\",
     "display_errors_details" => false
 ];
 
 const CONFIG_DISPLAY_ERROR_DETAILS = false;
 
-const CONFIG_URL = "https://meusite.com";
+const CONFIG_URL = "https://api.dominio.com";
 
